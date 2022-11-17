46fc6682ad37b269395a39e45d9c78e03311ce5f
Jordan <jdvandenbruel@gmail.com>
2022-11-17 08:24:08 -0700

    Social Footer Links Open In New Tab

--- src/components/contact/FooterSocials.tsx
+++ src/components/contact/FooterSocials.tsx
@@ -34,25 +34,41 @@ const FooterSocials = () => {
   const theme = useTheme();
   return (
     <StyledFooterSocials>
-      <a href="https://github.com/Jdvdb">
+      <a
+        href="https://github.com/Jdvdb"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <StyledSocial
           src={theme === lightTheme ? GithubLight : GithubDark}
           alt="Github Logo"
         />
       </a>
-      <a href="https://www.linkedin.com/in/jordan-van-den-bruel-324431144/">
+      <a
+        href="https://www.linkedin.com/in/jordan-van-den-bruel-324431144/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <StyledSocial
           src={theme === lightTheme ? LinkedinLight : LinkedInDark}
           alt="LinkedIn Logo"
         />
       </a>
-      <a href="https://www.instagram.com/jordan.vdb_/">
+      <a
+        href="https://www.instagram.com/jordan.vdb_/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <StyledSocial
           src={theme === lightTheme ? InstagramLight : InstagramDark}
           alt="Instagram Logo"
         />
       </a>
-      <a href="https://www.strava.com/athletes/39724994">
+      <a
+        href="https://www.strava.com/athletes/39724994"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <StyledSocial
           src={theme === lightTheme ? StravaLight : StravaDark}
           alt="Strava Logo"
